1d1788609d634d5f292ef7c5f5279fe270638049
hitheima <hitheima@163.com>
2019-06-04 16:15:53 +0800

    登录 - 参数化&封装 toast 方法&完善登录脚本

--- base/base_action.py
+++ base/base_action.py
@@ -1,3 +1,5 @@
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 
 
@@ -6,10 +8,10 @@ class BaseAction:
     def __init__(self, driver):
         self.driver = driver
 
-    def find_element(self, feature, timeout=10, poll=1):
+    def find_element(self, feature, timeout=10, poll=1.0):
         return WebDriverWait(self.driver, timeout, poll).until(lambda x: x.find_element(*feature))
 
-    def find_elements(self, feature, timeout=10, poll=1):
+    def find_elements(self, feature, timeout=10, poll=1.0):
         return WebDriverWait(self.driver, timeout, poll).until(lambda x: x.find_elements(*feature))
 
     def click(self, feature):
@@ -24,3 +26,28 @@ class BaseAction:
 
     def get_feature_text(self, feature):
         return self.find_element(feature).text
+
+    def is_toast_exist(self, message):
+        """
+        判断 界面上是否有 toast 的内容包含 message
+        :param message: toast的部分信息
+        :return:
+        """
+        toast_feature = By.XPATH, "//*[contains(@text, '%s')]" % message
+        try:
+            self.find_element(toast_feature, 5, 0.1)
+            return True
+        except TimeoutException:
+            return False
+
+    def get_toast_text(self, message):
+        """
+        根据 toast 的部分的 message 消息，获取所有的toast内容
+        :param message: toast的部分信息
+        :return:
+        """
+        if self.is_toast_exist(message):
+            toast_feature = By.XPATH, "//*[contains(@text, '%s')]" % message
+            return self.find_element(toast_feature, 5, 0.1).text
+        else:
+            raise Exception("没有找到 对应的toast")
--- scripts/test_login.py
+++ scripts/test_login.py
@@ -1,5 +1,7 @@
 import time
+import pytest
 
+from base.base_analyze import analyze_data
 from base.base_driver import init_driver
 from page.page import Page
 
@@ -10,17 +12,26 @@ class TestLogin:
         self.driver = init_driver()
         self.page = Page(self.driver)
 
-    def test_login(self):
+    @pytest.mark.parametrize("args", analyze_data("login_data", "test_login"))
+    def test_login(self, args):
+
+        username = args["username"]
+        password = args["password"]
+        toast = args["toast"]
+
         # 首页 点击 我
         self.page.home.click_me()
         # 注册 点击 已有账号去登陆
         self.page.register.click_login()
         # 登录 输入 用户名
-        self.page.login.input_username("itheima_test")
+        self.page.login.input_username(username)
         # 登录 输入 密码
-        self.page.login.input_password("itheima")
+        self.page.login.input_password(password)
         # 登录 点击 登录
         self.page.login.click_login()
 
-        assert self.page.me.get_username_text() == "itheima_test"
-        assert self.driver.current_activity != "com.yunmall.ymctoc.ui.activity.LogonActivity"
+        if toast is None:  # 如果 toast 为 none，表示 登录成功的断言
+            assert self.page.me.get_username_text() == "itheima_test"
+            assert self.driver.current_activity != "com.yunmall.ymctoc.ui.activity.LogonActivity"
+        else: # 如果 toast 有值，表示 预期失败，通过toast的内容断言
+            assert self.page.login.is_toast_exist(toast)
